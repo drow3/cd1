3d535053eb4ae7e13bc9d10d5a248941e9f4de54
drow3 <drow313@gmail.com>
2016-10-17 18:04:32 +0200

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,17 +1,13 @@
-from random import randint
-nev=input("Hogy hívnak?")
-print("szia " + nev)
-print("Gondoltam egy számra 1 és 100 között, találd ki!")
-x=randint(1,100) 
+import sys
 
-for n in range(1, 10):	
-	y=int(input(str(n) + ". tipp?"))
-	if y == x:
-		print("Király vagy!")
-		break
-	elif y < x:
-		print("A gondolt szám nagyobb")
-	else:
-		print("A gondolt szám kisebb")
+def kiiras(nev):
+	print('hello ' + nev)
 
-print(x)
+
+nev = 'vilag'
+if(len(sys.argv) == 2):
+	nev = sys.argv[1]
+elif(len(sys.argv) > 2):
+	print("Maximum 1 nev adhato meg.")
+	sys.exit(2)
+kiiras(nev)
